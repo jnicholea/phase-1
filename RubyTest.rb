f1351bc1e34801dd26a84c37cf10449428d7a4ff
Shawn Watson <shawnw86@gmail.com>
2015-08-18 08:41:52 -0700

    Add last name functionality for double the fun...ctionality

--- RubyTest.rb
+++ RubyTest.rb
@@ -5,3 +5,8 @@ name.reverse!
 puts "Your name backwards is #{name}"
 name.upcase!
 puts "Your name backwards and upcase is #{name}!"
+puts "Enter your last name:"
+lastname = gets.chomp
+name.reverse!
+name.capitalize!
+puts "Your name is #{name} #{lastname}"
