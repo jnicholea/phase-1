317b6bebdd609add07b664cd4e0ac8a035b0e8e9
Shawn Watson <shawnw86@gmail.com>
2015-08-18 08:30:50 -0700

    Add RubyTest.rb to repo

---
+++ RubyTest.rb
@@ -0,0 +1,7 @@
+puts "Enter your name:"
+name = gets.chomp
+puts "Your name is #{name}!"
+name.reverse!
+puts "Your name backwards is #{name}"
+name.upcase!
+puts "Your name backwards and upcase is #{name}!"
